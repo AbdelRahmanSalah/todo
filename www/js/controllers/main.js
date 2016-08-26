d2947e043360bf9bc2d140e6965c406c8f63ba2c
AbdelRahmanSalah <abdosalahh@gmail.com>
2016-08-26 08:45:18 +0200

    fix issue in tasks array manipulation

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -1,7 +1,7 @@
 app.controller('main', function ($scope, $ionicModal, localStorageService) { //store the entities name in a variable
 
 var taskData = 'task';
-
+// localStorageService.remove(taskData);
 //initialize the tasks scope with empty array
 $scope.tasks = [];
 
@@ -16,6 +16,16 @@ $ionicModal.fromTemplateUrl('new-task-modal.html', {
    $scope.newTaskModal = modal;
 });
 
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
 $scope.getTasks = function () {
   //fetches task from local storage
   if (localStorageService.get(taskData)) {
@@ -27,7 +37,8 @@ $scope.getTasks = function () {
 $scope.getCompletedTasks = function () {
   //fetches task from local storage
   if (localStorageService.get(taskData)) {
-      $scope.tasks = localStorageService.get(taskData).filter(function(task) {
+      $scope.tasks = localStorageService.get(taskData);
+      $scope.tasks = $scope.tasks.filter(function(task) {
         return task.completed;
       });
   } else {
@@ -48,21 +59,43 @@ $scope.getUnCompletedTasks = function () {
 
 $scope.createTask = function () {
    //creates a new task
+   $scope.task.guid = guid();
    $scope.tasks.push($scope.task);
-   localStorageService.set(taskData, $scope.tasks);
+   if(localStorageService.get(taskData)) {
+     var bridge = localStorageService.get(taskData);
+   } else {
+     var bridge = [];
+   }
+   bridge.push($scope.task);
+   localStorageService.set(taskData, bridge);
    $scope.task = {};
    //close new task modal
    $scope.newTaskModal.hide();
 
 }
-$scope.removeTask = function (index) {
+$scope.removeTask = function (guid) {
    //removes a task
-   $scope.tasks.splice(index, 1);
-   localStorageService.set(taskData, $scope.tasks);
+   $scope.tasks = $scope.tasks.filter(function(task) {
+     return task.guid != guid;
+   });
+   var bridge = localStorageService.get(taskData);
+   bridge = bridge.filter(function(task) {
+     return task.guid != guid;
+   });
+   localStorageService.set(taskData, bridge);
 }
-$scope.completeTask = function (index) {
+$scope.completeTask = function (guid) {
    //updates a task as completed
-  localStorageService.set(taskData, $scope.tasks);
+   var bridge = localStorageService.get(taskData);
+   bridge = bridge.map(function(task) {
+     if(task.guid == guid) {
+        task.completed = $scope.tasks.filter(function(task2){
+         return task.guid == guid;
+       })[0].completed;
+     }
+     return task;
+   });
+  localStorageService.set(taskData, bridge);
 }
 $scope.openTaskModal = function () {
   $scope.newTaskModal.show();
